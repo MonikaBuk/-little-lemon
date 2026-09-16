f1b97f310d7181a1b24cb0c122afe8ff9e36f36c
Monika Bukovics <monikab@roperrhodes.co.uk>
2026-09-16 14:40:18 +0100

    Upload Project

---
+++ LittleLemon/app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -0,0 +1,13 @@
+package com.example.littlelemon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+
+@Composable
+fun Onboarding() {
+    Column {
+        Text(text = "Let's get to know you")
+        // You will add TextFields and Button here next
+    }
+}
